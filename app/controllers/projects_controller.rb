fc1f374976a89cabc529e8d2d0fadeabf85b32a1
Shozo Yamakami <ya_makami@hotmail.co.jp>
2015-10-14 00:10:28 +0900

    second commit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,10 +15,14 @@ class ProjectsController < ApplicationController
     end
 
     def show
+        # コントローラでparams[:]で取ることができる。
     	@project = Project.find(params[:id])
     end
 
+    # *******************************************
+    # new,createはどちらかが書けることはできない
     def new
+        # 新しいProjectインスタンスを設定する。
     	@project = Project.new
     end
 
@@ -26,22 +30,41 @@ class ProjectsController < ApplicationController
     # 送信ボタンなど何か情報送るときに使われるアクション
     # createしたものを保存する
     def create
+        # フォームで入力されたものを入れてあげる必要がある。
         @project = Project.new(project_params)
-        # もしセーブすることができれば、projectに行き、
+        # 保存するときにValidationが発動する。
+        # もしセーブすることができれば、projectに行く
         if @project.save
             redirect_to projects_path
-            # もし値がなく、セーブすることができなければ、validatesのpresenceがtrueだったら、
         else
+            # もし値がなく、セーブすることができなければ、validatesのpresenceがtrueだったら
             render 'new'
         end
     end
 
-    # 決まり文句的なもの
+    # *******************************************
+    # edit,updateはどちらかが書けることはできない
+    def edit
+        @project = Project.find(params[:id])
+    end
+    def update
+        @project = Project.find(params[:id])
+        if @project.update(project_params)
+            redirect_to projects_path
+        else
+            render 'edit'
+        end
+
+    end
+    # *******************************************
+
+    # 決まり文句的なもの（代表的な手法）ー＞入力情報を管理する
     # プライベート関数にする、外部から呼び出すことができない。
     # 昔はこのようにしていなかったようであるが、セキュリティ上このようにするらしい
     # ここでフィルタリングして渡すというのが最近の手法らしい
     private
         def project_params
+            # 色々情報がる待っているparams[:project]の中の:titleだけを取る。
             params[:project].permit(:title)
         end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,9 @@
 # railsのモデルの部分
 class Project < ActiveRecord::Base
     # validarionを設定する
-    # 値が空でないか(presence)を検証する。
-    validates :title, presence: true
+    # 値が空でないか(presence)を検証す。
+    # :titleがからだったらダメ
+    validates :title,
+    presence: { message: "入力してください！"},
+    length: {minimum: 3, message: "短すぎ"}
 end
